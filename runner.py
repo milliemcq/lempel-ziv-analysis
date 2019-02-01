6ea80829d5b643f48e4a30fc27262caba9ae0af1
Your Name Here <millie@mcquillin.com>
2019-02-01 08:58:15 +0000

    Lookahead comparisons

--- runner.py
+++ runner.py
@@ -4,18 +4,20 @@ import time
 import sys
 
 
-
 sys.stdout = open("output.txt", "w")
 print("test sys.stdout")
 
-for i in range(1, 20000, 100):
-    for j in range(1, 20000, 100):
+i = 5000
 
-        print("-----------------------------NEW-------------------------")
-        print("Window: " + str(i))
-        print("Lookahead: " + str(j))
-        start = 0
-        start = time.time()
-        lz77_compressor("1984.txt", i, j)
-        finish = time.time()
-        print("Time taken = " + str(finish - start))
+for j in range(1, 1000, 10):
+
+    print("-----------------------------NEW-------------------------")
+    print("Window: " + str(i))
+    print("Lookahead: " + str(j))
+    start = 0
+    start = time.time()
+    lz77_compressor("1984.txt", i, j)
+    finish = time.time()
+    print("Time taken = " + str(finish - start))
+
+
