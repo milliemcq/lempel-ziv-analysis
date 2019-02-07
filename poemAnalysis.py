a963a649c9d54c8ec45752bf1d854a598d5caac1
Your Name Here <millie@mcquillin.com>
2019-02-07 11:49:07 +0000

    Lookahead analysis complete

---
+++ poemAnalysis.py
@@ -0,0 +1,25 @@
+from lempel_ziv_compressor import lz77_compressor
+from lempel_ziv_decompressor import lz77_decompressor
+import time
+import sys
+
+files = ["txt_files/1984.txt", "txt_files/alicewonder.txt"]
+
+sys.stdout = open("outputs/NEWLOOKAHEADS.txt", "w")
+print("test sys.stdout")
+
+for item in files:
+    print(item)
+    print("####################################### NEW FILE ###################################")
+    i = 5000
+
+    for j in range(1, 200, 10):
+
+        print("-----------------------------NEW-------------------------")
+        print("Window: " + str(i))
+        print("Lookahead: " + str(j))
+        start = 0
+        start = time.time()
+        lz77_compressor(item, i, j)
+        finish = time.time()
+        print("Time taken = " + str(finish - start))
